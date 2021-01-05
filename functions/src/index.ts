0f5daf0c87e99b9c5f05f27f64c891d43710c76b
Santiago Valencia <edy-santiago@outlook.com>
2021-01-04 23:00:14 -0500

    get doc

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,14 +1,22 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 
-var serviceAccount = require("./serviceAccountkey.json");
+const serviceAccount = require("./serviceAccountkey.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+const db = admin.firestore();
+
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.json({ message: "Hola mundo desde funciones de Firebase!!!!" });
+});
+
+export const getGOTY = functions.https.onRequest(async (request, response) => {
+  // const nombre = request.query.nombre || 'sin nombre';
+  const gotyRef = db.collection("goty");
+  const docsSnap = await gotyRef.get();
+  const juegos = docsSnap.docs.map((doc) => doc.data());
+
+  response.json(juegos);
 });
