a3b1e3dabc4e71baf7d46b3c410cadfdfa7dd9fb
Santiago Valencia <edy-santiago@outlook.com>
2021-01-04 23:29:30 -0500

    install express

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import * as express from "express";
+import * as cors from "cors";
 
 const serviceAccount = require("./serviceAccountkey.json");
 admin.initializeApp({
@@ -20,3 +22,20 @@ export const getGOTY = functions.https.onRequest(async (request, response) => {
 
   response.json(juegos);
 });
+
+//Express
+//npm instal express cors  ----folder functions
+//npm install @types/express --save-dev
+//npm install @types/cors --save-dev
+
+const app = express();
+app.use(cors({ origin: true }));
+app.get("/goty", async (req, res) => {
+  const gotyRef = db.collection("goty");
+  const docsSnap = await gotyRef.get();
+  const juegos = docsSnap.docs.map((doc) => doc.data());
+
+  res.json(juegos);
+});
+
+export const api = functions.https.onRequest(app);
