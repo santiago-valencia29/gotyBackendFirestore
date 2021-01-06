d0ff2b619e907bc29ef892cd7772522c8cb4c1bf
Santiago Valencia <edy-santiago@outlook.com>
2021-01-05 21:06:10 -0500

    incrementar voto a un juego

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,7 @@ import * as express from "express";
 import * as cors from "cors";
 
 const serviceAccount = require("./serviceAccountkey.json");
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
@@ -30,12 +31,35 @@ export const getGOTY = functions.https.onRequest(async (request, response) => {
 
 const app = express();
 app.use(cors({ origin: true }));
+
 app.get("/goty", async (req, res) => {
   const gotyRef = db.collection("goty");
   const docsSnap = await gotyRef.get();
   const juegos = docsSnap.docs.map((doc) => doc.data());
 
   res.json(juegos);
+});
+
+app.post("/goty/:id", async (req, res) => {
+  const id = req.params.id;
+  const gameRef = db.collection("goty").doc(id);
+  const gameSnap = await gameRef.get();
+
+  if (!gameSnap.exists) {
+    res.status(404).json({
+      ok: false,
+      mensaje: "No existe un juego con ese ID" + id,
+    });
+  } else {
+    const antes = gameSnap.data() || { votos: 0 }; //validacion para eslint || {votoss:0}
+    gameRef.update({
+      votos: antes.votos + 1,
+    });
+    res.json({
+      ok: true,
+      mensaje: `Gracias por tu voto a ${antes.name}`,
+    });
+  }
 });
 
 export const api = functions.https.onRequest(app);
